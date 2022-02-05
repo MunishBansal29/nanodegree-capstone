35d99322ec740c6b60c4df22f8b572ac47abb810
munishbansal29 <munish0029@gmail.com>
2022-02-05 10:02:48 +0530

    screenshots

--- app/app.py
+++ app/app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 def index():
     try:
         host_name = socket.gethostname()
-        host_ip = socket.gethostbyname(host_name)
+        host_ip = socket.gethostbyId(host_name)
 
         return render_template('index.html', hostname=host_name, ip=host_ip)
     except:
         return render_template('error.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
